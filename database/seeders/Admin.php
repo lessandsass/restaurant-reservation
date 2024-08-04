3faa4d95846d51695c93a2c83f0f9faf9ea9f480
Less <31947204+lessandsass@users.noreply.github.com>
2024-08-04 07:56:37 +0530

    Admin seeder
    The Admin seeder was created and the data for admin user was added.
    The Admin seeder class was called in the DatabaseSeeder class to create the Admin user.

---
+++ database/seeders/Admin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class Admin extends Seeder
+{
+
+    protected static ?string $password;
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => static::$password ??= Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
