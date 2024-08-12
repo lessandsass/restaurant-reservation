01e60d613788b8410b65d3f8a200fbbf8958834a
Less <31947204+lessandsass@users.noreply.github.com>
2024-08-12 21:16:21 +0530

    logout link and admin link
    Logout form element was copied from the layouts\navigation.blade.php file and pasted in the dropdown.
    Admin Link was validated and allowed to show if the authenticated user is admin in the navigation.blade.php file.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,13 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    @if(Auth::user()->is_admin)
+                        <x-nav-link :href="route('admin.index')" :active="request()->routeIs('admin.index')">
+                            {{ __('Admin') }}
+                        </x-nav-link>
+                    @endif
+
                 </div>
             </div>
 
@@ -48,6 +55,7 @@
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
                         </form>
+
                     </x-slot>
                 </x-dropdown>
             </div>
