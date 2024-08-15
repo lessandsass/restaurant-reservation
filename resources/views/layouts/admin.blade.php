90dc5521728f334f779f58226082d95f892ac6af
Less <31947204+lessandsass@users.noreply.github.com>
2024-08-15 16:26:48 +0530

    Active link
    admin-nav-link.blade.php file was created inside the components directory by duplicating the nav-link file.
    
    The class code block was added by copying old active link code which was available in the template for active and other links.
    
    The links was replaced by the validated links by copying from the navigation.blade.php file whichi is inside the layouts directory.

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -34,33 +34,21 @@
 
                 <nav :class="{'block': open, 'hidden': !open}" class="flex-grow px-4 pb-4 md:block md:pb-0 md:overflow-y-auto">
 
-                    <a
-                        class="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-gray-200 rounded-lg dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                        href="{{ route('admin.categories.index') }}"
-                    >
-                        Categories
-                    </a>
+                    <x-admin-nav-link :href="route('admin.categories.index')" :active="request()->routeIs('admin.categories.index')">
+                        {{ __('Admin Categories') }}
+                    </x-admin-nav-link>
 
-                    <a
-                        class="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg dark:bg-transparent dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                        href="{{ route('admin.menus.index') }}"
-                    >
-                        Menus
-                    </a>
+                    <x-admin-nav-link :href="route('admin.menus.index')" :active="request()->routeIs('admin.menus.index')">
+                        {{ __('Admin Menus') }}
+                    </x-admin-nav-link>
 
-                    <a
-                        class="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg dark:bg-transparent dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                        href="{{ route('admin.tables.index') }}"
-                    >
-                        Tables
-                    </a>
+                    <x-admin-nav-link :href="route('admin.tables.index')" :active="request()->routeIs('admin.tables.index')">
+                        {{ __('Admin Tables') }}
+                    </x-admin-nav-link>
 
-                    <a
-                        class="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg dark:bg-transparent dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                        href="{{ route('admin.reservations.index') }}"
-                    >
-                        Reservations
-                    </a>
+                    <x-admin-nav-link :href="route('admin.reservations.index')" :active="request()->routeIs('admin.reservations.index')">
+                        {{ __('Admin Reservation') }}
+                    </x-admin-nav-link>
 
                     <div @click.away="open = false" class="relative" x-data="{ open: false }">
 
