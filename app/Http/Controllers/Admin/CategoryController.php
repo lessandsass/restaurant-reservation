eb808029d324c0c50cef06e8518e720c29dbeea6
Less <31947204+lessandsass@users.noreply.github.com>
2024-08-18 01:58:02 +0530

    Form validation, create and retrive data from DB
    Fillable properties were added in the Category.php model.
    CategoryStoreRequest.php was created with the artisan command.
    CategoryStoreRequest was implemented with the syntax
    The return value of the authorise method was change to true.
    The validation rules for form fields were added in the rules method.
    Then the store method of the CategoryController was implemented with the help of the CategoryStoreRequest.
    The image was stored into the `public/categories` directory. The data row was created and the page redirected to the index page.
    The data was retrived and showed in the index page.

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryStoreRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -19,9 +20,18 @@ class CategoryController extends Controller
         return view('admin.categories.create');
     }
 
-    public function store(Request $request)
+    public function store(CategoryStoreRequest $request)
     {
-        //
+        $image = $request->file('image')->store('public/categories');
+
+        Category::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'image' => $image,
+        ]);
+
+        return to_route('admin.categories.index');
+
     }
 
     public function show(string $id)
--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -20,26 +20,31 @@
             <div class="m-2 p-2">
                 {{-- Copied from the link that is provided in the tutorial --}}
                 <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10 text-white">
-                    <form enctype="multipart/form-data">
+                    <form
+                        method="POST"
+                        action="{{ route('admin.categories.store') }}"
+                        enctype="multipart/form-data"
+                    >
+                        @csrf
 
                         <div class="sm:col-span-6">
-                            <label for="title" class="block text-sm font-medium text-gray-700"> Name </label>
+                            <label for="name" class="block text-sm font-medium text-gray-700"> Name </label>
                             <div class="mt-1">
-                                <input type="text" id="title" wire:model.lazy="title" name="title" class="block w-full transition duration-150 ease-in-out appearance-none bg-slate-600 border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                <input type="text" id="name" name="name" class="block w-full appearance-none bg-slate-600 border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                             </div>
                         </div>
 
                         <div class="sm:col-span-6">
-                            <label for="title" class="block text-sm font-medium text-gray-700"> Image </label>
+                            <label for="image" class="block text-sm font-medium text-gray-700"> Image </label>
                             <div class="mt-1">
-                                <input type="file" id="image" wire:model="newImage" name="image" class="block w-full transition duration-150 ease-in-out appearance-none bg-slate-600 border border-gray-400 rounded-md py-2 px-3 text-base text-white leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                <input type="file" id="image" name="image" class="block w-full appearance-none bg-slate-600 border border-gray-400 rounded-md py-2 px-3 text-base text-white leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                             </div>
                         </div>
 
                         <div class="sm:col-span-6 pt-5">
-                            <label for="body" class="block text-sm font-medium text-gray-700">Description</label>
+                            <label for="description" class="block text-sm font-medium text-gray-700">Description</label>
                             <div class="mt-1">
-                                <textarea id="body" rows="3" wire:model.lazy="body" class="shadow-sm focus:ring-indigo-500 appearance-none bg-slate-600 border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"></textarea>
+                                <textarea name="description" id="description" rows="3" class="shadow-sm focus:ring-indigo-500 appearance-none bg-slate-600 border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm"></textarea>
                             </div>
                         </div>
 
