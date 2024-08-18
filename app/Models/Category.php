c04eef072ce1926b0515f0c8d6c22f3d91fdbd31
Less <31947204+lessandsass@users.noreply.github.com>
2024-08-18 14:15:47 +0530

    Pivot table, relationships and category dropdown
    Pivot table between the Category and Menu models using the artisan command.
    
    The table columns were added create_category_menu_table.php
    
    All the category values were grabbed in the create method in the Admin\MenuController
    
    Show the categories in the dropdown menu
    
    The relationships were implemented

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -11,4 +11,9 @@ class Category extends Model
 
     protected $fillable = ['name', 'description', 'image'];
 
+    public function menus()
+    {
+        return $this->belongsToMany(Menu::class, 'category_menu');
+    }
+
 }
--- resources/views/admin/menus/create.blade.php
+++ resources/views/admin/menus/create.blade.php
@@ -1,7 +1,7 @@
 <x-admin-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Categories') }}
+            {{ __('Menus') }}
         </h2>
     </x-slot>
 
@@ -25,21 +25,34 @@
                         <div class="sm:col-span-6">
                             <label for="title" class="block text-sm font-medium text-gray-700"> Name </label>
                             <div class="mt-1">
-                                <input type="text" id="title" wire:model.lazy="title" name="title" class="block w-full transition duration-150 ease-in-out appearance-none bg-slate-600 border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                <input type="text" id="title" name="title" class="block w-full transition duration-150 ease-in-out appearance-none bg-slate-600 border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                             </div>
                         </div>
 
                         <div class="sm:col-span-6">
                             <label for="title" class="block text-sm font-medium text-gray-700"> Image </label>
                             <div class="mt-1">
-                                <input type="file" id="image" wire:model="newImage" name="image" class="block w-full transition duration-150 ease-in-out appearance-none bg-slate-600 border border-gray-400 rounded-md py-2 px-3 text-base text-white leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                <input type="file" id="image" name="image" class="block w-full transition duration-150 ease-in-out appearance-none bg-slate-600 border border-gray-400 rounded-md py-2 px-3 text-base text-white leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                             </div>
                         </div>
 
                         <div class="sm:col-span-6 pt-5">
                             <label for="body" class="block text-sm font-medium text-gray-700">Description</label>
                             <div class="mt-1">
-                                <textarea id="body" rows="3" wire:model.lazy="body" class="shadow-sm focus:ring-indigo-500 appearance-none bg-slate-600 border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"></textarea>
+                                <textarea id="body" rows="3" class="shadow-sm focus:ring-indigo-500 appearance-none bg-slate-600 border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"></textarea>
+                            </div>
+                        </div>
+
+                        <div class="sm:col-span-6 pt-5">
+                            <label for="body" class="block text-sm font-medium text-gray-700">Categories</label>
+                            <div class="mt-1">
+                                <select class="w-full bg-slate-600 rounded-lg">
+                                    @foreach($categories as $category)
+                                        <option value="">
+                                            {{ $category->name }}
+                                        </option>
+                                    @endforeach
+                                </select>
                             </div>
                         </div>
 
