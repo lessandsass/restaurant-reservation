8ba4eb31386fc42a44b4ed716f6c67c3d6233282
Less <31947204+lessandsass@users.noreply.github.com>
2024-08-25 15:57:30 +0530

    background-image overlay
    add some css properties to apply background overlay.

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,13 +1,13 @@
 <div
-    class="container max-w-lg px-4 py-32 mx-auto text-left bg-center bg-no-repeat bg-cover md:max-w-none md:text-center"
-    style="background-image: url('https://cdn.pixabay.com/photo/2016/11/18/14/39/beans-1834984_960_720.jpg')">
+    class="container max-w-lg px-4 py-32 mx-auto text-left bg-center bg-no-repeat bg-cover md:max-w-none md:text-center bg-gray-500"
+    style="background:linear-gradient(0deg, rgba(4, 4, 94, 0.7), rgba(113, 12, 12, 0.7)), url(https://cdn.pixabay.com/photo/2016/11/18/14/39/beans-1834984_960_720.jpg); background-size:cover; min-height:100%;"
+>
     <h1
         class="font-mono text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 md:text-center sm:leading-none lg:text-5xl">
         <span class="inline md:block">Welcome To Larainfo Restaurant</span>
     </h1>
     <div class="mx-auto mt-2 text-green-50 md:text-center lg:text-lg">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta voluptatem ab necessitatibus illo praesentium
-        culpa excepturi quae commodi quaerat,
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta voluptatem ab illo praesentium culpa excepturi quae commodi quaerat,
     </div>
     <div class="flex flex-col items-center mt-12 text-center">
         <span class="relative inline-flex w-full md:w-auto">
